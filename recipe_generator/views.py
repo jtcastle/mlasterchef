8243a6939da7434c82183a472860682bcff371ae
jtcastle <56699410+jtcastle@users.noreply.github.com>
2022-03-15 21:06:47 -0400

    recipe_generator app is funcitoning with a hello world page

--- recipe_generator/views.py
+++ recipe_generator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from django.template import loader
+
+from django.views.generic import TemplateView, ListView
+# from .models import Products
+# from django.db.models import Q
 
 # Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'recipe_generator/home.html'
