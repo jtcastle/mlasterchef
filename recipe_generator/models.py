01dda56219b23261a0e549e4c28ddca4d360585c
Phoebe Piercy <phoebepi@mit.edu>
2022-03-29 20:38:50 +0100

    :)

--- recipe_generator/models.py
+++ recipe_generator/models.py
@@ -16,9 +16,9 @@ class IngredientList(models.Model):
     tag = models.CharField(max_length = 50)
     ing_list = models.CharField(max_length = 400)
     complete = models.BooleanField("End of Ingredients?")
-    recipe = models.CharField(max_length = 850, default="Recipe_Placeholder")
-    output_ingredients = models.CharField(max_length = 850, default="ing_Placeholder")
-    output_steps = models.CharField(max_length = 850, default="steps_Placeholder")
+    recipe = models.TextField(default="Recipe_Placeholder")
+    output_ingredients = models.TextField(default="ing_Placeholder")
+    output_steps = models.TextField(default="steps_Placeholder")
 
     def get_recipe(self):
         self.recipe = get_recipe_with_string_input(self.ing_list, self.complete)
