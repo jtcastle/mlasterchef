1cc726bb6c710f06a552320f8c09e6472e87404c
Phoebe Piercy <phoebepi@mit.edu>
2022-03-31 14:13:17 +0100

    format

--- recipe_generator/forms.py
+++ recipe_generator/forms.py
@@ -7,6 +7,11 @@ from django import forms
 #         model = IngredientList
 #         fields = ('tag', 'ing_list', 'complete',)
 
+TRUE_FALSE_CHOICES = (
+    (True, 'Yes'),
+    (False, 'No')
+)
+
 class IngredientListForm(forms.ModelForm):
     class Meta:
         model = IngredientList
@@ -14,12 +19,13 @@ class IngredientListForm(forms.ModelForm):
         labels = {
             "tag": "",
             "ing_list": "",
-            "complete": "Check the box if you would like the generator to limit the recipe to the ingredients listed"
+            "complete": "Would like the generator to limit the recipe to the ingredients listed"
         }
     
     # I dont really know what this does but it works and allows me to add placeholders
     def __init__(self, *args, **kwargs):
         super(IngredientListForm, self).__init__(*args, **kwargs)
         
+        self.fields['complete'].widget = forms.Select(choices=TRUE_FALSE_CHOICES)
         self.fields['tag'].widget.attrs['placeholder'] = 'Please enter a name for your recipe'
         self.fields['ing_list'].widget.attrs['placeholder'] = 'Please enter a comma separated list of ingredients'
