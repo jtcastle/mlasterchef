0ba84975d5c65110a863d9441065223c4ddbf2ec
Phoebe Piercy <phoebe@Phoebes-Air.attlocal.net>
2022-06-15 20:43:48 -0700

    bug fix

--- recipe_generator/views.py
+++ recipe_generator/views.py
@@ -18,14 +18,14 @@ class HomePageView(TemplateView):
     def get_queryset(self):
         return IngredientList.objects.all
 
-class OutputPageView(ListView):
-    model = IngredientList
-    template_name = 'recipe_generator/output.html'
-    context_object_name = 'ing_list_obj'
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        return query
+#class OutputPageView(ListView):
+#    model = IngredientList
+#    template_name = 'recipe_generator/output.html'
+#    context_object_name = 'ing_list_obj'
+#
+#    def get_queryset(self):
+#        query = self.request.GET.get('q')
+#        return query
 
 #    def get_queryset(self):
 #        for i in IngredientList.objects.all():
@@ -43,14 +43,15 @@ def home(request):
             messages.success(request, ('Your ingredients were successfully added!'))
         else:
             messages.error(request, 'Error saving form')
-        return redirect('loading')
+        latest_id = IngredientList.objects.last().id
+        return redirect('loading', latest_id)
     
     ing_form = IngredientListForm()
     saved_ingredients_list = IngredientList.objects.all()
     return render(request=request, template_name='recipe_generator/home.html', context={'ing_form':ing_form, 'saved_ingredients_list':saved_ingredients_list})
 
-def output(request):
-    ing_list_obj = IngredientList.objects.last()
+def output(request, ing_list_obj_id):
+    ing_list_obj = get_object_or_404(IngredientList, pk=ing_list_obj_id)
     ing_list_obj.get_recipe()
     ing_list_obj.save()
     return redirect('recipe', ing_list_obj.id)
@@ -58,9 +59,10 @@ def output(request):
 def recipe(request, ing_list_obj_id):
     ing_list_obj =  get_object_or_404(IngredientList, pk=ing_list_obj_id)
     if (request.GET.get('refreshbtn')):
-        ing_list_obj.get_recipe()
-        ing_list_obj.save()
-        return redirect('recipe', ing_list_obj.id)
+        return redirect('loading', ing_list_obj.id)
+        #ing_list_obj.get_recipe()
+        #ing_list_obj.save()
+        #return redirect('recipe', ing_list_obj.id)
     
     if (request.GET.get('complete_btn')):
         ing_list_obj.complete = not ing_list_obj.complete
@@ -70,8 +72,8 @@ def recipe(request, ing_list_obj_id):
     return render(request, 'recipe_generator/output.html', {'ing_list_obj': ing_list_obj,})
 
 
-def loading(request):
-    return render(request, 'recipe_generator/loading.html')
+def loading(request, ing_list_obj_id):
+    return render(request, 'recipe_generator/loading.html', context={'ing_list_obj_id': ing_list_obj_id})
 
 class SearchResultsView(ListView):
     model = IngredientList
